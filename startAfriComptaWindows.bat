102b23ddfc1fcaf1734d4d7508299b407483ff9e
Linus Gasser <ineiti@linusetviviane.ch>
2014-11-11 21:58:59 +0100

    Windows internationalization of 'date'

--- startAfriComptaWindows.bat
+++ startAfriComptaWindows.bat
@@ -1,16 +1,20 @@
 rem To start AfriCompta
 
-set DB=test
+set DB=sam
 set PATH=%PATH%;Windows
 del sqlite3-ruby-1.2.5\lib\sqlite3_api.so
 copy Windows\reloader.rb camping-1.5\lib\camping
 md Backup
 md Log
 
-set D=%date:/=.%
-set T1=%time: =0%
-set T2=%T1::=_%
-set T=%D%-%T2:~0,8%
+for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set "dt=%%a"
+set "YYYY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%"
+set "Min=%dt:~10,2%"
+set "Sec=%dt:~12,2%"
+set T=%YYYY%-%MM%-%DD%_%HH%.%Min%.%Sec%
 set LOG=Log\ac-%T%.log
 
 Windows\7z a Backup\%DB%-%T%.zip db.%DB%
