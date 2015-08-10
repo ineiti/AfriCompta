6b0f805ca87d28f8c6465bb35854c05409129327
Linus Gasser <ineiti@linusetviviane.ch>
2015-08-10 08:19:56 +0200

    Started work on macos-app packaging

--- bin/africompta.sh
+++ bin/africompta.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd $( dirname ${BASH_SOURCE[0]} )
 echo "DATA_DIR=$HOME/Library/AfriCompta" > ../afri_compta.conf
-./afri_compta.rb
+./afri_compta.rb &
+sleep 5
 open -a Safari.app http://localhost:3302
 read a
