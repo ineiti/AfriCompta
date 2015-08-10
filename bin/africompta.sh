2635325e57486b5e37b703fd869b20c95e75b7f2
Linus Gasser <ineiti@linusetviviane.ch>
2015-08-10 11:35:04 +0200

    Started work on macos-app packaging

--- bin/africompta.sh
+++ bin/africompta.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 cd $( dirname ${BASH_SOURCE[0]} )
 echo "DATA_DIR=$HOME/Library/AfriCompta" > ../afri_compta.conf
-PATH="../../ruby/bin:$PATH"
+#PATH="../../ruby/bin:$PATH"
 echo $PATH
 ruby --version
 which ruby
+pkill -f "afri_compta.rb"
 ./afri_compta.rb &
-#AFRICOMPTA=$!
+AFRICOMPTA=$!
 sleep 5
+echo $AFRICOMPTA
 open -Wn -a Safari.app http://localhost:3302
 #kill $!
 #open -a Safari.app http://localhost:3302/$!
