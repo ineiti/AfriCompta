5c6e1a198498c3438f5a00322af9118b9f9f1332
Linus Gasser <ineiti@linusetviviane.ch>
2015-08-10 08:09:29 +0200

    Started work on macos-app packaging

--- bin/africompta.sh
+++ bin/africompta.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-WDIR=$( dirname ${BASH_SOURCE[0]} )
-pwd > /tmp/apppwd
-echo $WDIR >> /tmp/apppwd
-
-$WDIR/../Resources/AfriCompta/afri_compta.rb
+cd $( dirname ${BASH_SOURCE[0]} )
+./afri_compta.rb
 read a
