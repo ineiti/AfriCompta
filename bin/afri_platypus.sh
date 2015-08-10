8e6f35eb1d6cfa27a7442f7766b96df70049507b
Linus Gasser <ineiti@linusetviviane.ch>
2015-08-10 07:47:16 +0200

    Started work on macos-app packaging

--- bin/afri_platypus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd AfriCompta/bin
-./afri_compta.rb
-echo "<a href='http://localhost:3301'>Going to AfriCompta</a>"
---
+++ bin/africompta.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+WDIR=$( dirname ${BASH_SOURCE[0]} )
+pwd > /tmp/apppwd
