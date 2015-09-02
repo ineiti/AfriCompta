ab25a9f029284ee21db7b1ae7fc9870d2c556122
Linus Gasser <ineiti@linusetviviane.ch>
2015-09-02 07:42:13 +0200

    Refining editing of accounts

--- lib/africompta/views/compta/edit_accounts.rb
+++ lib/africompta/views/compta/edit_accounts.rb
@@ -14,14 +14,16 @@ class ComptaEditAccounts < View
       gui_vbox :nogroup do
         show_str :name
         show_str :desc, :width => 300
-        #show_list_drop :multiplier, '%w(passive active)'
-        #show_list_drop :keep_total, '%w(true false)'
+        show_list_drop :multiplier, '%w(passive active)'
+        show_list_drop :keep_total, '%w(true false)'
         show_button :save, :account_update
       end
 
       gui_window :account_new do
         show_str :name_new, :width => 300
         show_str :desc_new
+        show_list_drop :multiplier_new, '%w(passive active)'
+        show_list_drop :keep_total_new, '%w(true false)'
         show_button :add_account, :close
       end
 
@@ -38,19 +40,31 @@ class ComptaEditAccounts < View
     end
   end
 
+  def set_mult_keep(acc, mult, keep)
+    acc.multiplier = mult.first == 'active' ? -1 : 1
+    acc.keep_total = keep.first == 'true'
+  end
+
+  def get_mult_keep(acc)
+    [acc.multiplier == -1 ? 'active' : 'passive',
+     acc.keep_total ? 'true' : 'false']
+  end
+
   def rpc_button_save(session, data)
     if (acc = data._account_list).class == Account
       acc.desc, acc.name = data._desc, data._name
-      #acc.multiplier = data._multiplier.first == 'active' ? -1 : 1
-      #acc.keep_total = data._keep_total.first == 'true'
+      set_mult_keep(acc, data._multiplier, data._keep_total)
       update_list
     end
   end
 
   def rpc_button_new(session, data)
-    if data._account_list.class == Account
+    if (acc = data._account_list).class == Account
+      mult, keep = get_mult_keep(acc)
       reply(:window_show, :account_new) +
-          reply(:empty, %w( name_new desc_new ))
+          reply(:empty, %w( name_new desc_new )) +
+          reply(:update, multiplier_new: [mult]) +
+          reply(:update, keep_total_new: [keep])
     else
       reply(:window_show, :msg_win) +
           reply(:update, :msg => "Choisir un compte parent d'abord")
@@ -61,6 +75,7 @@ class ComptaEditAccounts < View
     if (acc = data._account_list).class == Account
       return unless data._name_new.to_s.length > 0
       new_acc = Accounts.create(data._name_new, data._desc_new, acc)
+      set_mult_keep(new_acc, data._multiplier_new, data._keep_total_new)
       update_list(data._account_archive) +
           reply(:update, :account_list => new_acc.id) +
           reply(:window_hide)
@@ -106,13 +121,12 @@ class ComptaEditAccounts < View
   def rpc_list_choice_account_list(session, data)
     reply(:empty_nonlists) +
         if (acc = data._account_list) != []
-          mult = acc.multiplier == -1 ? 'active' : 'passive'
-          kt = acc.keep_total ? 'true' : 'false'
+          mult, kt = get_mult_keep(acc)
           reply(:update, {total: acc.total_form,
                           desc: acc.desc,
                           name: acc.name,
-                          multiplier: mult,
-                          keep_total: kt})
+                          multiplier: [mult],
+                          keep_total: [kt]})
         else
           []
         end
